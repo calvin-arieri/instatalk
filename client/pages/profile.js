a59e76b16da1c1581794b6cc372b46198596ddc5
QUANDEVV <vercelploy@gmail.com>
2023-07-04 07:42:30 +0300

    .

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>This is where the user will post,,edit,,,delete
-    <h1>CRUD OPERATIONS</h1>
+    <h1>CRUD OPERATIONS </h1>
     </div>
    
   )
