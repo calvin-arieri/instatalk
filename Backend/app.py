1f5569d3b6c4c8bcb30ba8dd0de0b8c880218e8c
AyimWilliam <odhisayim@gmail.com>
2023-07-04 14:03:42 +0300

    database created

--- Backend/app.py
+++ Backend/app.py
@@ -1,23 +1,21 @@
 # server/app.py
 from flask import Flask, jsonify, request, make_response
-from flask_bcrypt import Bcrypt
+
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_cors import CORS
 
-from models import db, User, Post, Comment
+from models import db, User, Post, Comment,bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instatalk.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-# instantiate Bcrypt with app instance
-bcrypt = Bcrypt(app)
-
 CORS(app)
 migrate = Migrate(app, db)
 db.init_app(app)
+bcrypt.init_app(app)
 
 api=Api(app)
 
--- Backend/models.py
+++ Backend/models.py
@@ -2,9 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql import func
-from app import bcrypt
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
