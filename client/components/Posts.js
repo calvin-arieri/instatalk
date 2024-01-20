3259bfc46d2547bc98223590639d525655db886f
calvin-arieri <126255829+calvin-arieri@users.noreply.github.com>
2024-01-21 00:24:42 +0300

    Update Posts.js

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -51,7 +51,7 @@ const Posts = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5555/users')
+    fetch('https://instatok.onrender.com/users')
       .then((response) => response.json())
       .then((data) => setUsers(data));
   }, []);
